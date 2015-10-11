be6e74d578771f5ae42e8a0f8eb6cf9a7cdbd860
yjq13 <281882327@qq.com>
2015-10-12 01:49:02 +0800

    部分修改和添加缓存存储机制

---
+++ Classes/CacheData.cpp
@@ -0,0 +1,9 @@
+//
+//  CacheData.cpp
+//  PrisonBreak_v1.1
+//
+//  Created by yjq131250077 on 10/12/15.
+//
+//
+
+#include "CacheData.h"
---
+++ Classes/CacheData.h
@@ -0,0 +1,22 @@
+//
+//  CacheData.hpp
+//  PrisonBreak_v1.1
+//
+//  Created by yjq131250077 on 10/12/15.
+//
+//
+
+#ifndef CacheData_h
+#define CacheData_h
+
+#include <stdio.h>
+
+class CacheData{
+public:
+    int GOLD_COIN_GET = 0;
+    int DIAMOND_GET = 1000;
+};
+
+static CacheData CACHEINFO;
+
+#endif /* CacheData_h*/
