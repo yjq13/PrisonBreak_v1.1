576b48658e10e2551ac2bbc68f5e7c713b1c55f9
yjq13 <281882327@qq.com>
2015-10-23 23:36:10 +0800

    use修改

--- Classes/Constant_Use.cpp
+++ Classes/Constant_Use.cpp
@@ -12,3 +12,13 @@ cocos2d::TransitionPageTurn* Constant_Use::createTransition_Page(cocos2d::Scene*
     cocos2d::TransitionPageTurn* transition=cocos2d::TransitionPageTurn::create(1.5f, sceneNew, false);
     return transition;
 }
+
+bool Constant_Use::init(){
+    VISIBLE_SIZE = cocos2d::Director::getInstance()->getVisibleSize();
+    
+    ORIGIN = cocos2d::Director::getInstance()->getVisibleOrigin();
+    
+    DESIGN_SIZE=Size(1334.0,750.0);
+    
+    return true;
+}
--- Classes/Constant_Use.h
+++ Classes/Constant_Use.h
@@ -12,13 +12,17 @@
 USING_NS_CC;
 class Constant_Use{
 public:
-    cocos2d::Size VISIBLE_SIZE = cocos2d::Director::getInstance()->getVisibleSize();
+    bool init();
     
-    cocos2d::Vec2 ORIGIN = cocos2d::Director::getInstance()->getVisibleOrigin();
+    cocos2d::Size VISIBLE_SIZE;
+    
+    cocos2d::Vec2 ORIGIN;
+    
+    
     
     cocos2d::TransitionPageTurn* createTransition_Page(cocos2d::Scene* sceneNew);
     
-    cocos2d::Size DESIGN_SIZE=Size(1334.0,750.0);
+    cocos2d::Size DESIGN_SIZE;
     
 };
 static Constant_Use CONSTANT_USE;
